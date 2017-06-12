0c60ff32596b97d52a7210aa721e9c364ed923fd
Xiao-Y <lyongtao123@126.com>
2017-06-12 11:22:24 +0800

    4444

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,7 +3,7 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(1111884444);
-		System.out.println(9999999);
+		System.out.println(888888);
+		System.out.println(444444);
 	}
 }
