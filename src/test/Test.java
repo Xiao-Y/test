10618ea823c049bd23fe02e55f36f9215834a4cb
Xiao-Y <lyongtao123@126.com>
2017-06-12 11:42:10 +0800

    0--

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println(888888);
 		System.out.println(888888);
+		System.out.println(666666);
 		System.out.println(99999);
+		System.out.println(000000);
 	}
 }
