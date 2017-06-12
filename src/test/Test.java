89688d16d7ac4c9466011d19b55c7b3bea3ffb68
Xiao-Y <lyongtao123@126.com>
2017-06-12 11:16:09 +0800

    冲突

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println(1111884444);
-		System.out.println(1111884444);
-		System.out.println(1111884444);
+		System.out.println(9999999);
+		System.out.println(888888);
 	}
 }
